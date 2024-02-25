b93d8b817221e60969deef064daa9bb396c9bb05
uzair <uzairchaudhary.30@gmail.com>
2024-02-25 18:49:32 +0500

    product model

--- pages/api/cart.js
+++ pages/api/cart.js
@@ -1,5 +1,5 @@
 import {mongooseConnect} from "@/lib/mongoose"
-import {Product} from "@/models/Product"
+import {Product} from "@/models/product"
 
 export default async function handle(req,res){
     await mongooseConnect();
--- pages/api/checkout.js
+++ pages/api/checkout.js
@@ -1,5 +1,5 @@
 import {mongooseConnect} from '@/lib/mongoose'
-import {Product} from '@/models/Product'
+import {Product} from '@/models/product'
 import { Order } from '../../models/order';
 
 
